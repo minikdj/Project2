e8bccdccd8403b089131aed7d1c3a3ae772e6632
minikdj <minikdj11@gmail.com>
2019-04-29 11:14:42 -0400

    fixed some given code

--- source/PerFragmentOperations.cpp
+++ source/PerFragmentOperations.cpp
@@ -8,13 +8,9 @@ fogType FragmentOperations::fogSetting = NO_FOG;
 
 void FragmentOperations::processFragment(Fragment & fragment)
 {
-	// Set the color for the pixel 
-    if (frameBuffer.getDepth((int)fragment.windowPosition.x, (int)fragment.windowPosition.y) > fragment.windowPosition.z) {
-
-
-    }
     // Set the color for the pixel 
-    frameBuffer.setPixel((int)fragment.windowPosition.x, (int)fragment.windowPosition.y, fragment.shadedColor);
-
-    frameBuffer.setDepth((int)fragment.windowPosition.x, (int)fragment.windowPosition.y, fragment.windowPosition.z);
+    if (frameBuffer.getDepth((int)fragment.windowPosition.x, (int)fragment.windowPosition.y) > fragment.windowPosition.z) {
+        frameBuffer.setPixel((int)fragment.windowPosition.x, (int)fragment.windowPosition.y, fragment.shadedColor);
+        frameBuffer.setDepth((int)fragment.windowPosition.x, (int)fragment.windowPosition.y, fragment.windowPosition.z);
+    }
 } // end processFragment
